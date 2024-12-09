fc5baadf8339bb679c5e6972db13ba3a0daf8c66
lasto-ed12 <70526829+eo33@users.noreply.github.com>
2024-12-10 00:32:48 +1100

    added procfile

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -40,6 +40,7 @@ const config: Config = {
           routeBasePath: "/",
           sidebarPath: "./sidebars.ts",
           docItemComponent: "@theme/ApiItem",
+
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
