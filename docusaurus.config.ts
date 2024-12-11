88b21ef1e86ddc63a8172c80da14b2ab4f2883b7
lasto-ed12 <70526829+eo33@users.noreply.github.com>
2024-12-11 12:04:19 +1100

    added sitemap

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   favicon: "img/logo192.png",
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-site.example.com",
+  url: "https://docs4.thefurniturebros.com/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
@@ -45,6 +45,12 @@ const config: Config = {
         blog: false,
         theme: {
           customCss: "./src/css/custom.css",
+        },
+        sitemap: {
+          lastmod: "date",
+          changefreq: "yearly",
+          priority: 0.5,
+          filename: "sitemap.xml",
         },
       } satisfies Preset.Options,
     ],
