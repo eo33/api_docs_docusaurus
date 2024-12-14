c5722e1e724a43e574c1f0eda6ef13d779957398
lasto-ed12 <70526829+eo33@users.noreply.github.com>
2024-12-14 15:57:42 +1100

    clear cache

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -102,15 +102,6 @@ const config: Config = {
         },
       },
     ],
-    [
-      "docusaurus-biel",
-      {
-        enable: true,
-        project: "g8qfq12rzf",
-        headerTitle: "The Furniture Bros API Chatbot",
-        version: "latest",
-      },
-    ],
   ],
   themes: ["docusaurus-theme-openapi-docs"], // export theme components
 
